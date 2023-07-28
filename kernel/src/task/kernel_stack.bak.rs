2d12ff1c3fc037ee0bc4258bc612e62112401f20
melody <CelestialMelodyGo@gmail.com>
2023-07-29 00:29:52 +0800

    引入process后kernel_stack的更改

---
+++ kernel/src/task/kernel_stack.bak.rs
@@ -0,0 +1,50 @@
+///! 进程内核栈
+use crate::{
+    consts::{KERNEL_STACK_SIZE, PAGE_SIZE, SIGNAL_TRAMPOLINE},
+    mm::{kernel_vmm::acquire_kvmm, memory_set::VmAreaType, MapPermission, VirtAddr},
+};
+
+use super::PidHandle;
+
+/// Return (bottom, top) of a kernel stack in kernel space.
+pub fn kernel_stack_position(id: usize) -> (usize, usize) {
+    let top = SIGNAL_TRAMPOLINE - id * (KERNEL_STACK_SIZE + PAGE_SIZE);
+    let bottom = top - KERNEL_STACK_SIZE;
+
+    (bottom, top)
+}
+
+/// 进程内核栈
+pub struct KernelStack {
+    pid: usize,
+}
+
+impl KernelStack {
+    /// 从一个已分配的进程标识符中对应生成一个内核栈
+    pub fn new(pid_handle: &PidHandle) -> Self {
+        let pid = pid_handle.0;
+        let (kernel_stack_bottom, kernel_stack_top) = kernel_stack_position(pid);
+        acquire_kvmm().insert_framed_area(
+            kernel_stack_bottom.into(),
+            kernel_stack_top.into(),
+            MapPermission::R | MapPermission::W,
+            VmAreaType::KernelStack,
+        );
+
+        KernelStack { pid: pid_handle.0 }
+    }
+
+    /// 获取当前应用内核栈顶在内核地址空间中的地址(这地址仅与app_id有关)
+    pub fn top(&self) -> usize {
+        let (_, kernel_stack_top) = kernel_stack_position(self.pid);
+        kernel_stack_top
+    }
+}
+
+impl Drop for KernelStack {
+    fn drop(&mut self) {
+        let (kernel_stack_bottom, _) = kernel_stack_position(self.pid);
+        let kernel_stack_bottom_va: VirtAddr = kernel_stack_bottom.into();
+        acquire_kvmm().remove_area_with_start_vpn(kernel_stack_bottom_va.into());
+    }
+}
--- kernel/src/task/kernel_stack.rs
+++ kernel/src/task/kernel_stack.rs
@@ -1,51 +1,51 @@
 //! 进程内核栈
 
+use spin::RwLock;
+
 use crate::{
-    consts::{KERNEL_STACK_SIZE, PAGE_SIZE, SIGNAL_TRAMPOLINE},
+    consts::{KERNEL_STACK_SIZE, PAGE_SIZE, TRAMPOLINE},
     mm::{kernel_vmm::acquire_kvmm, memory_set::VmAreaType, MapPermission, VirtAddr},
+    task::task::RecycleAllocator,
 };
 
-use super::PidHandle;
+lazy_static! {
+    static ref KSTACK_ALLOCATOR: RwLock<RecycleAllocator> =
+        unsafe { RwLock::new(RecycleAllocator::new()) };
+}
 
 /// Return (bottom, top) of a kernel stack in kernel space.
-pub fn kernel_stack_position(id: usize) -> (usize, usize) {
-    let top = SIGNAL_TRAMPOLINE - id * (KERNEL_STACK_SIZE + PAGE_SIZE);
+pub fn kernel_stack_position(kstack_id: usize) -> (usize, usize) {
+    let top = TRAMPOLINE - kstack_id * (KERNEL_STACK_SIZE + PAGE_SIZE);
     let bottom = top - KERNEL_STACK_SIZE;
-
     (bottom, top)
 }
 
-/// 进程内核栈
-pub struct KernelStack {
-    pid: usize,
-}
+pub struct KernelStack(pub usize);
 
-impl KernelStack {
-    /// 从一个已分配的进程标识符中对应生成一个内核栈
-    pub fn new(pid_handle: &PidHandle) -> Self {
-        let pid = pid_handle.0;
-        let (kernel_stack_bottom, kernel_stack_top) = kernel_stack_position(pid);
-        acquire_kvmm().insert_framed_area(
-            kernel_stack_bottom.into(),
-            kernel_stack_top.into(),
-            MapPermission::R | MapPermission::W,
-            VmAreaType::KernelStack,
-        );
+pub fn kstack_alloc() -> KernelStack {
+    let kstack_id = KSTACK_ALLOCATOR.write().alloc();
+    let (kstack_bottom, kstack_top) = kernel_stack_position(kstack_id);
 
-        KernelStack { pid: pid_handle.0 }
-    }
-
-    /// 获取当前应用内核栈顶在内核地址空间中的地址(这地址仅与app_id有关)
-    pub fn top(&self) -> usize {
-        let (_, kernel_stack_top) = kernel_stack_position(self.pid);
-        kernel_stack_top
-    }
+    acquire_kvmm().insert_framed_area(
+        kstack_bottom.into(),
+        kstack_top.into(),
+        MapPermission::R | MapPermission::W,
+        VmAreaType::KernelStack,
+    );
+    KernelStack(kstack_id)
 }
 
 impl Drop for KernelStack {
     fn drop(&mut self) {
-        let (kernel_stack_bottom, _) = kernel_stack_position(self.pid);
+        let (kernel_stack_bottom, _) = kernel_stack_position(self.0);
         let kernel_stack_bottom_va: VirtAddr = kernel_stack_bottom.into();
         acquire_kvmm().remove_area_with_start_vpn(kernel_stack_bottom_va.into());
+    }
+}
+
+impl KernelStack {
+    pub fn top(&self) -> usize {
+        let (_, kernel_stack_top) = kernel_stack_position(self.0);
+        kernel_stack_top
     }
 }
