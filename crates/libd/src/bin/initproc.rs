a2ab7077a32f2581b90238fb872b481d4d1da2b3
sslime336 <sslime@yeah.net>
2023-07-29 21:00:00 +0800

    mess2

--- crates/libd/src/bin/initproc.rs
+++ crates/libd/src/bin/initproc.rs
@@ -22,7 +22,7 @@ fn main() -> isize {
         );
     } else {
         let mut exit_code = 0_i32;
-        let res = waitpid(pid as usize, &mut exit_code);
+        let res = waitpid(-1, &mut exit_code);
         println!("child proc exit_code: {}", exit_code);
         println!("waitpid result: {}", res);
     }
